7b1261eda797accfff7805875e25db11901d3570
AO Industries, Inc <support@aoindustries.com>
2021-06-29 03:03:20 -0500

    Now supports Java 9+ modules.

---
+++ src/main/java/module-info.java
@@ -0,0 +1,27 @@
+/*
+ * ao-tempfiles - Java temporary file API filling-in JDK gaps and deficiencies.
+ * Copyright (C) 2021  AO Industries, Inc.
+ *     support@aoindustries.com
+ *     7262 Bull Pen Cir
+ *     Mobile, AL 36695
+ *
+ * This file is part of ao-tempfiles.
+ *
+ * ao-tempfiles is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * ao-tempfiles is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with ao-tempfiles.  If not, see <http://www.gnu.org/licenses/>.
+ */
+module com.aoapps.tempfiles {
+	exports com.aoapps.tempfiles;
+	// Java SE
+	requires java.logging;
+}
